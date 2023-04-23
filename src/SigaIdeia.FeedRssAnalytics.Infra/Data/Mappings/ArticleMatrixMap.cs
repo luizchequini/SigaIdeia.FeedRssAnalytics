03e82b32304fc35a693a3e1e2811037a10b3e020
Luiz Chequini <luizchequini@gmail.com>
2023-04-23 13:05:29 -0300

    Criação do ArticleMatrixMap para mapear a entidade de Artigos

---
+++ src/SigaIdeia.FeedRssAnalytics.Infra/Data/Mappings/ArticleMatrixMap.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SigaIdeia.FeedRssAnalytics.Domain.Entities;
+
+namespace SigaIdeia.FeedRssAnalytics.Infra.Data.Mappings
+{
+    public class ArticleMatrixMap : IEntityTypeConfiguration<ArticleMatrix>
+    {
+        public void Configure(EntityTypeBuilder<ArticleMatrix> builder)
+        {
+            builder.HasKey(e => e.Id);
+            builder.Property(e => e.AuthorId).HasColumnType("varchar(100)").HasColumnName("AuthorId").IsRequired();
+            builder.Property(e => e.Author).HasColumnType("varchar(150)").IsRequired();
+            builder.Property(e => e.Link).HasColumnType("varchar(max)").IsRequired();
+            builder.Property(e => e.Title).HasColumnType("varchar(300)").IsRequired();
+            builder.Property(e => e.Type).HasColumnType("varchar(50)").IsRequired();
+            builder.Property(e => e.Category).HasColumnType("varchar(50)").IsRequired();
+            builder.Property(e => e.Views).HasColumnType("varchar(max)");
+        }
+    }
+}
